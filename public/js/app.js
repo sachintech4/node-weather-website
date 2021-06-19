ee60c7cf3179341f155d8c82d906922902081a18
Sandeep <sandeeptech8@gmail.com>
2021-06-19 14:47:03 +0530

    Unncessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log('Client side JS is loaded')
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message1')
